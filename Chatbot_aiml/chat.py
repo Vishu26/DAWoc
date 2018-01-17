99471f0fc40ea72b30a77b28f89e4573be954f81
Srikumar Sastry <srikumarsastry1997@gmail.com>
2018-01-17 16:57:01 +0530

    Add files via upload

---
+++ Chatbot_aiml/chat.py
@@ -0,0 +1,20 @@
+import aiml
+import warnings
+from random import choice
+
+warnings.filterwarnings("ignore")
+
+kernel = aiml.Kernel()
+kernel.learn("startup.xml")
+kernel.respond("load aiml b")
+
+
+
+while True:
+    a = kernel.respond(input("Srikumar >> ").upper())
+    if not a:
+        print(''.join(choice(['Was that a typo?',
+                              "I am afraid, I can't understand",
+                              'Am I overfitted on data?, coz I can\'t get that one!',
+                              'Correct your grammar probably?'])))
+    print(a)
